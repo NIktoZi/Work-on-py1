860dfb0916e5e8b934e6809d800ba37ba919e18b
NIktoZi <bajkovnikita@yandex.ru>
2022-07-14 00:14:56 +0300

    3

--- W1.py
+++ W1.py
@@ -12,12 +12,26 @@
 #         print('День недели не является выходным')
 
 # №2 
-print('x,y,z  результат 1    результат 2')
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            res= not (x or y or z)
-            res2= not (x) or not (y) or not (z)
-            print(x,y,z,'  ',res,'   ',res2)
-            if res ==res2:
-                print('Верное равенство')
+# print('x,y,z  результат 1    результат 2')
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             res= not (x or y or z)
+#             res2= not (x) or not (y) or not (z)
+#             print(x,y,z,'  ',res,'   ',res2)
+#             if res ==res2:
+#                 print('Верное равенство')
+#№3
+x=int(input('Введите кординату X '))
+y=int(input('Введите кординату Y '))
+if x==0 and y==0:
+    print('Недопустимое значение')
+elif x>0 and y>0:
+    print('Первая четверть')
+elif x>0 and y<0:
+    print('Четветрая четверть')
+elif x<0 and y>0:
+    print('Вторая четверть')
+elif x<0 and y<0:
+    print('Третья четверть')
+
